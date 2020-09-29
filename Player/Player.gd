f2fc3d6717cb3b572beb2fad040e5834f90c858b
Joseph Choe <joseph@josephchoe.com>
2020-09-29 01:18:13 -0600

    Player animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,9 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
 func _ready():
 	pass
 
@@ -17,8 +20,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
